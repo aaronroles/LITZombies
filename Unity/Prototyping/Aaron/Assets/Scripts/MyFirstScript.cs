914d6665c2265bd6c6fcd91deb8511acd645ad41
Aaron Roles <aaron_roles93@hotmail.com>
2016-02-01 10:25:23 +0000

    Unity prototyping

---
+++ Unity/Prototyping/Aaron/Assets/Scripts/MyFirstScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyFirstScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		var testArray = new string[2];
+		testArray [0] = "Aaron";
+		testArray [1] = "Roles";
+		var myName = testArray [0] + " " + testArray [1];
+		Debug.Log (myName);
+		print (myName);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
