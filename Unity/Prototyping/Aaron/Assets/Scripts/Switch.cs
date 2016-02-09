1507907dc76458160b15ed14c87d3f1d6c10533f
Aaron Roles <aaron_roles93@hotmail.com>
2016-02-09 22:04:31 +0000

    Prototyping: Interactive switch object

---
+++ Unity/Prototyping/Aaron/Assets/Scripts/Switch.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Switch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay2D(Collider2D target){
+		if(Input.GetKey(KeyCode.Space))
+			if (target.gameObject.tag == "Player"){
+				print ("Hit the switch");
+				Destroy(gameObject);
+		}
+	}
+}
