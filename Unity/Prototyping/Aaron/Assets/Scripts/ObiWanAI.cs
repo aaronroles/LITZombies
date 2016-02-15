dd351072d2f3dc3e5ff689afcc02737af6ffa2c6
Aaron Roles <aaron_roles93@hotmail.com>
2016-02-15 19:04:06 +0000

    Prototyping: enemy AI

---
+++ Unity/Prototyping/Aaron/Assets/Scripts/ObiWanAI.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObiWanAI : MonoBehaviour {
+
+	private Vector3 player;
+	private Vector2 playerDirection;
+	private float xDiff;
+	private float yDiff;
+	public float speed;
+	private float distance;
+
+	// Use this for initialization
+	void Start () {
+		speed = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		player = GameObject.FindWithTag("Player").transform.position;
+		xDiff = player.x - transform.position.x;
+		yDiff = player.y - transform.position.y;
+		playerDirection = new Vector2 (xDiff, yDiff);
+		distance = Vector2.Distance (player, transform.position);
+		//Debug.Log (distance);
+
+		if (distance < 1) {
+			//Debug.Log("Distance is "+ distance);
+			rigidbody2D.AddForce (playerDirection.normalized * speed);
+		}
+	}
+}
