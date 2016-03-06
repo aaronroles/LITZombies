039139115929edc54aa8fe29a74febdeaa1c8e34
Aaron Roles <aaron_roles93@hotmail.com>
2016-03-06 21:41:06 +0000

    Got Richie's animations working with WASD keys

---
+++ Unity/Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public Vector2 moving = new Vector2();
+	
+	// Update is called once per frame
+	void Update () {
+		moving.x = moving.y = 0;
+
+		if(Input.GetKey(KeyCode.A)){
+			print("a");
+			moving.x = -1;
+		}
+		else if(Input.GetKey(KeyCode.D)){
+			print("d");
+			moving.x = 1;
+		}
+
+		if (Input.GetKey (KeyCode.W)) {
+			print("w");
+			moving.y = 1;
+		}
+		else if(Input.GetKey(KeyCode.S)){
+			print("s");
+			moving.y = -1;
+		}
+	}
+}
---
+++ Unity/Game/Assets/Scripts/Richie_Player.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Richie_Player : MonoBehaviour {
+
+	public float speed = 10f;
+	public Vector2 maxVelocity = new Vector2(3,3);
+	private PlayerController controller;
+	private Animator anim;
+
+	void Start () {
+		controller = GetComponent<PlayerController> ();
+		anim = GetComponent<Animator> ();
+	}
+	
+	void Update () {
+		var forceX = 0f;
+		var forceY = 0f;
+		var absVelX = Mathf.Abs (rigidbody2D.velocity.x);
+		var absVelY = Mathf.Abs (rigidbody2D.velocity.y);
+
+		if (controller.moving.x != 0) {
+			if (absVelX < maxVelocity.x) {
+				forceX = speed * controller.moving.x;
+				if (controller.moving.x == 1) {
+					anim.SetInteger ("AnimState", 1);
+					transform.localScale = new Vector3 (1, 1, 1);
+					print ("Right");
+				} 
+				else if (controller.moving.x == -1) {
+					anim.SetInteger ("AnimState", 1);
+					transform.localScale = new Vector3 (-1, 1, 1);
+					print ("Left");
+				}
+			}
+		}
+
+		else if (controller.moving.y != 0) {
+			if (absVelY < maxVelocity.y) {
+				forceY = speed * controller.moving.y;
+				if (controller.moving.y == 1) {
+					anim.SetInteger ("AnimState", 2);
+					print ("Up");
+				} 
+				else if (controller.moving.y == -1) {
+					anim.SetInteger ("AnimState", 3);
+					print ("Down");
+				}
+			}
+		}
+
+		else {
+			anim.SetInteger("AnimState", 0);
+		}
+
+		rigidbody2D.AddForce(new Vector2(forceX, forceY));
+	}
+}
