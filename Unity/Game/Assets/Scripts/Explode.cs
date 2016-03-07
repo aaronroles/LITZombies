860649ab5f45ea8114d8bf8f310421fa7d1705e1
sinead.71 <cooneysinead71.com>
2016-03-07 21:41:12 +0000

    Prototyping the zombie exploding when he dies. Ran into an error. Will come back to it.

---
+++ Unity/Game/Assets/Scripts/Explode.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explode : MonoBehaviour {
+
+	public BodyPart bodyPart;
+	public int totalParts = 5;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D t){
+		if (t.gameObject.tag == "Deadly") { 
+			OnExplode();
+			print ("test");
+		}
+	}
+
+	void OnExplode(){
+		Destroy (gameObject);
+
+		var t = transform;
+
+		for (int i = 0; i < totalParts; i++) {
+			t.TransformPoint(0, -100, 0);
+			Zombie_BodyPart clone = Instantiate(bodyPart, t.position, Quaternion.identity) as BodyPart;
+		}
+	}
+
+}
