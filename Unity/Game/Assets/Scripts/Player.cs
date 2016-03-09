53f05171319e30ffaf49525e47a2ecb96377c82e
Aaron Roles <aaron_roles93@hotmail.com>
2016-03-09 16:32:29 +0000

    Working on attack animation

--- Unity/Game/Assets/Scripts/Player.cs
+++ Unity/Game/Assets/Scripts/Player.cs
@@ -19,52 +19,44 @@ public class Player : MonoBehaviour {
 		var absVelX = Mathf.Abs (rigidbody2D.velocity.x);
 		var absVelY = Mathf.Abs (rigidbody2D.velocity.y);
 
-		/*if (controller.attack) {
+		if (controller.attack) {
+			Debug.Log("Attack!");
+			controller.moving.x = controller.moving.y = 0;
 			anim.SetTrigger("Attack");
-			if(controller.moving.x == 1){
-				print ("right");
-			}
-			else if (controller.moving.x == -1){
-				print ("left");
-			}
+		} 
 
-			if(controller.moving.y == 1){
-				print ("up");
-			}
-			else if (controller.moving.y == -1){
-				print ("down");
-			}
-		}*/
-
-		if (controller.moving.x != 0) {
-			if (absVelX < maxVelocity.x) {
-				forceX = speed * controller.moving.x;
-				if (controller.moving.x == 1) {
-					anim.SetInteger ("AnimState", 1);
-					transform.localScale = new Vector3 (1, 1, 1);
-				} 
-				else if (controller.moving.x == -1) {
-					anim.SetInteger ("AnimState", 1);
-					transform.localScale = new Vector3 (-1, 1, 1);
+		else{
+			Debug.Log("Not attacking");
+			if (controller.moving.x != 0) {
+				if (absVelX < maxVelocity.x) {
+					forceX = speed * controller.moving.x;
+					if (controller.moving.x == 1) {
+						anim.SetInteger ("AnimState", 1);
+						transform.localScale = new Vector3 (1, 1, 1);
+					} 
+					else if (controller.moving.x == -1) {
+						anim.SetInteger ("AnimState", 1);
+						transform.localScale = new Vector3 (-1, 1, 1);
+					}
 				}
 			}
-		}
-
-		else if (controller.moving.y != 0) {
-			if (absVelY < maxVelocity.y) {
-				forceY = speed * controller.moving.y;
-				if (controller.moving.y == 1) {
-					anim.SetInteger ("AnimState", 2);
-				} 
-				else if (controller.moving.y == -1) {
-					anim.SetInteger ("AnimState", 3);
+			
+			else if (controller.moving.y != 0) {
+				if (absVelY < maxVelocity.y) {
+					forceY = speed * controller.moving.y;
+					if (controller.moving.y == 1) {
+						anim.SetInteger ("AnimState", 2);
+					} 
+					else if (controller.moving.y == -1) {
+						anim.SetInteger ("AnimState", 3);
+					}
 				}
 			}
-		}
-
-		else {
-			anim.SetInteger("AnimState", 0);
-			transform.localScale = new Vector3 (1, 1, 1);
+			
+			else {
+				anim.SetInteger("AnimState", 0);
+				//transform.localScale = new Vector3 (1, 1, 1);
+			}
 		}
 
 		rigidbody2D.AddForce(new Vector2(forceX, forceY));
