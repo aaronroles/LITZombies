d5b627b4e355d077796b1dd870cc80831102a687
Aaron Roles <aaron_roles93@hotmail.com>
2016-03-14 18:58:47 +0000

    Made a manager script to handle spawning enemies (uses temp sprite for now), and started Bernie's animations

---
+++ Unity/Game/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnManager : MonoBehaviour {
+
+	public GameObject enemy;
+	public float spawnTime = 0.1f;
+	public Transform[] spawnPoints;
+
+	void Start () {
+		InvokeRepeating ("Spawn", spawnTime, spawnTime);
+	}
+
+	void Spawn(){
+		int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+		Instantiate (enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+	} 
+}
