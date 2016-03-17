a65db5985516afa5e9783c81da2b6e5d6bd65d99
Aaron Roles <aaron_roles93@hotmail.com>
2016-03-17 14:06:04 +0000

    Finished all of Richie's attack animations and hitbox areas!

--- Unity/Game/Assets/Scripts/Player.cs
+++ Unity/Game/Assets/Scripts/Player.cs
@@ -7,13 +7,17 @@ public class Player : MonoBehaviour
 	public Vector2 maxVelocity = new Vector2 (3, 3);
 	private PlayerController controller;
 	private Animator anim;
-	public Collider2D attackTrigger0;
+	public Collider2D attackTriggerX;
+	public Collider2D attackTriggerYup;
+	public Collider2D attackTriggerYdown;
 
 	void Start ()
 	{
 		controller = GetComponent<PlayerController> ();
 		anim = GetComponent<Animator> ();
-		attackTrigger0.enabled = false;
+		attackTriggerX.enabled = false;
+		attackTriggerYup.enabled = false;
+		attackTriggerYdown.enabled = false;
 	}
 	
 	void Update ()
@@ -22,10 +26,17 @@ public class Player : MonoBehaviour
 		var forceY = 0f;
 		var absVelX = Mathf.Abs (rigidbody2D.velocity.x);
 		var absVelY = Mathf.Abs (rigidbody2D.velocity.y);
-		anim.SetBool("Attack", false);
+
+		anim.SetBool("AttackX", false);
+		anim.SetBool("AttackY", false);
+		anim.SetBool("Attack0", false);
+
+		attackTriggerX.enabled = false;
+		attackTriggerYup.enabled = false;
+		attackTriggerYdown.enabled = false;
 
 		if (controller.moving.x != 0 && controller.attack == false) {
-			attackTrigger0.enabled = false;
+			attackTriggerX.enabled = false;
 			if (absVelX < maxVelocity.x) {
 				forceX = speed * controller.moving.x;
 				if (controller.moving.x == 1) {
@@ -40,21 +51,23 @@ public class Player : MonoBehaviour
 		} 
 
 		else if (controller.moving.x != 0 && controller.attack == true) {
-			attackTrigger0.enabled = true;
+			attackTriggerX.enabled = true;
 			if (absVelX < maxVelocity.x) {
 				forceX = speed * controller.moving.x;
 				if (controller.moving.x == 1) {
-					anim.SetBool("Attack", true);
+					anim.SetBool("AttackX", true);
 					transform.localScale = new Vector3 (1, 1, 1);
 				} 
 				else if (controller.moving.x == -1) {
-					anim.SetBool("Attack", true);
+					anim.SetBool("AttackX", true);
 					transform.localScale = new Vector3 (-1, 1, 1);
 				}
 			}
 		}
 
-		else if (controller.moving.y != 0) {
+		else if (controller.moving.y != 0 && controller.attack == false) {
+			attackTriggerYup.enabled = false;
+			attackTriggerYdown.enabled = false;
 			if (absVelY < maxVelocity.y) {
 				forceY = speed * controller.moving.y;
 				if (controller.moving.y == 1) {
@@ -65,6 +78,30 @@ public class Player : MonoBehaviour
 				}
 			}
 		} 
+
+		else if (controller.moving.y != 0 && controller.attack == true) {
+			if (absVelY < maxVelocity.y) {
+				forceY = speed * controller.moving.y;
+				if (controller.moving.y == 1) {
+					attackTriggerYdown.enabled = true;
+					anim.SetBool("AttackY", true);
+				} 
+				else if (controller.moving.y == -1) {
+					attackTriggerYup.enabled = true;
+					anim.SetBool("AttackY", true);
+				}
+			}
+		} 
+
+		else if((controller.moving.x == 0 && controller.moving.y == 0) && !controller.attack){
+			anim.SetInteger ("AnimState", 0);
+			attackTriggerX.enabled = false;
+		}
+
+		else if((controller.moving.x == 0 && controller.moving.y == 0) && controller.attack){
+			anim.SetBool("Attack0", true);
+			attackTriggerX.enabled = true;
+		}
 
 		else {
 			anim.SetInteger ("AnimState", 0);
