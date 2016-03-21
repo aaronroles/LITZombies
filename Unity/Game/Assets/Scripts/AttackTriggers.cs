2b9802178635a00327153a656a3c74c77488b305
Aaron Roles <aaron_roles93@hotmail.com>
2016-03-21 21:09:01 +0000

    Player health, blood particles

--- Unity/Game/Assets/Scripts/AttackTriggers.cs
+++ Unity/Game/Assets/Scripts/AttackTriggers.cs
@@ -3,12 +3,18 @@ using System.Collections;
 
 public class AttackTriggers : MonoBehaviour {
 
-	public int attackDmg = 2;
+	public int playerAttack = 2;
+	public int zombieAttack = 5;
 	private int count = 1;
 
-	void OnTriggerEnter2D(Collider2D zombie){
-		if (zombie.gameObject.tag == "Enemy") {
-			zombie.GetComponent<ZombieHealthManager>().Damage(attackDmg);
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.tag == "Enemy") {
+			other.GetComponent<ZombieHealthManager>().Damage(playerAttack);
+			//print (count++);
+		}
+
+		if (other.gameObject.tag == "Player") {
+			other.GetComponent<PlayerHealthManager>().Damage(zombieAttack);
 			//print (count++);
 		}
 	}
---
+++ Unity/Game/Assets/Scripts/BloodSplats.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BloodSplats : MonoBehaviour {
+
+	public Blood blood;
+	public int totalBlood = 1;
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.tag == "Player") {
+			Blood();
+		}
+	}
+	
+	void Blood(){
+		var t = transform;
+		for (int i = 0; i < totalBlood; i++) {
+			t.TransformPoint(0,-100,0);
+			Blood clone = Instantiate(blood, t.position, Quaternion.identity) as Blood;
+			clone.rigidbody2D.AddForce(Vector3.right * Random.Range (-50, 50));
+		}
+	}
+}
