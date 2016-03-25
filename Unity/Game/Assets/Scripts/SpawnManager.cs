e6b9e93415bc4492e4a6292d9b4676de87a01c2a
Aaron Roles <aaron_roles93@hotmail.com>
2016-03-25 14:23:36 +0000

    Still working on the Round text

--- Unity/Game/Assets/Scripts/SpawnManager.cs
+++ Unity/Game/Assets/Scripts/SpawnManager.cs
@@ -7,7 +7,6 @@ public class SpawnManager : MonoBehaviour {
 	public Transform[] spawnPoints;
 	public static int spawnCounter = 3;
 	private int spawnCount = 3;
-	private int round = 1;
 
 	void Start () {
 		// On Start, call spawn
@@ -18,23 +17,15 @@ public class SpawnManager : MonoBehaviour {
 		if (spawnCounter == 0) {
 			spawnCount += 2;
 			spawnCounter = spawnCount;
-			round++;
+			RoundManager.round++;
 			Spawn ();
 		}
 	}
 
 	void Spawn(){
-		StartCoroutine(Round(round, 2));
 		for (int i=0; i<spawnCounter; i++) {
 			int spawnPointIndex = Random.Range (0, spawnPoints.Length);
 			Instantiate (enemy, spawnPoints [spawnPointIndex].position, spawnPoints [spawnPointIndex].rotation);
 		}
-	}
-
-	IEnumerator Round(float roundNo, float delay){
-		guiText.text = "Round " + roundNo;
-		guiText.enabled = true;
-		yield return new WaitForSeconds(delay);
-		guiText.enabled = false;
 	}
 }
