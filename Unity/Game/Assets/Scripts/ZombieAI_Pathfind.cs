8025e8bd6417b2e50f3cb8b8ffdf0b26f88b3ce4
Aaron Roles <aaron_roles93@hotmail.com>
2016-04-05 22:14:59 +0100

    Worked a bit more on the pathfinding AI

--- Unity/Game/Assets/Scripts/ZombieAI_Pathfind.cs
+++ Unity/Game/Assets/Scripts/ZombieAI_Pathfind.cs
@@ -5,15 +5,16 @@ using System.Collections;
 public class ZombieAI_Pathfind : MonoBehaviour {
 
 	private KDNav2DAgent m_agent;
-	private Vector3 player;
+	private Vector2 noPlayer;
 	private Vector2 playerDirection;
 	private float playerX;
 	private float playerY;
-	private float speed;
-	private float distance;
+	private Animator animator;
+	private Vector2 lastPosition;
+	private Vector2 currentPosition;
 
-	void Start () {
-		//speed = (Random.Range (3, 7));
+	void Start(){
+		animator = GetComponent<Animator> ();
 	}
 
 	public KDNav2DAgent Zombie
@@ -28,20 +29,66 @@ public class ZombieAI_Pathfind : MonoBehaviour {
 		}
 		// If the game is not paused
 		else if(Time.timeScale == 1){
+			currentPosition = gameObject.transform.position;
 			// If the player object exists
 			if (GameObject.FindWithTag ("Player")) {
 				// Find it's position and track it
 				playerX = GameObject.FindWithTag ("Player").transform.position.x;
 				playerY = GameObject.FindWithTag ("Player").transform.position.y;
 				playerDirection = new Vector2 (playerX, playerY);
-				//rigidbody2D.AddForce (playerDirection.normalized * speed);
-				Zombie.SetDestination(playerDirection);
-				//print (m_agent + "" + playerDirection);
+
+				// If current x position is greater than last x position
+				// Going to the right
+				if(currentPosition.x > lastPosition.x){
+					animator.SetBool("ZombieStill", false);
+					animator.SetBool("ZombieSideWalk", true);
+					animator.SetBool("ZombieFrontWalk", false);
+					animator.SetBool("ZombieBackWalk", false);
+					transform.localScale = new Vector3(1,1,1); // Flips zombie to the right
+				}
+				
+				// If current x position is less than last x position
+				// Going to the left
+				if(currentPosition.x < lastPosition.x){
+					animator.SetBool("ZombieStill", false);
+					animator.SetBool("ZombieSideWalk", true);
+					animator.SetBool("ZombieFrontWalk", false);
+					animator.SetBool("ZombieBackWalk", false);
+					transform.localScale = new Vector3(-1,1,1); // Flips zombie to the left
+				}
+				
+				// If current y position is greater than last y position
+				// Going to the right
+				if(currentPosition.y < lastPosition.y){
+					animator.SetBool("ZombieStill", false);
+					animator.SetBool("ZombieSideWalk", false);
+					animator.SetBool("ZombieFrontWalk", true);
+					animator.SetBool("ZombieBackWalk", false);
+				}
+				
+				// If current y position is less than last y position
+				// Going to the right
+				if(currentPosition.y > lastPosition.y){
+					animator.SetBool("ZombieStill", false);
+					animator.SetBool("ZombieSideWalk", false);
+					animator.SetBool("ZombieFrontWalk", false);
+					animator.SetBool("ZombieBackWalk", true);
+				}
+				
+				if(currentPosition == lastPosition){
+					animator.SetBool("ZombieStill", true);
+					animator.SetBool("ZombieSideWalk", false);
+					animator.SetBool("ZombieFrontWalk", false);
+					animator.SetBool("ZombieBackWalk", false);
+				}
 			}
+			Zombie.SetDestination(playerDirection);
+			lastPosition = currentPosition;
 		} 
 		// Otherwise, no player position
 		else{	
-			player.x = player.y = player.z = 0;
+			noPlayer.x = noPlayer.y = 0;
+			Zombie.SetDestination(noPlayer);
 		}
 	}
 }
