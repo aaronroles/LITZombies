a5f83a87b58f8692c1bb43554f5279bf81240df0
sinead.71 <cooneysinead71.com>
2016-04-09 22:14:45 +0100

    Trying to fix the projectile.

--- Unity/Game/Assets/Scripts/Projectile.cs
+++ Unity/Game/Assets/Scripts/Projectile.cs
@@ -3,9 +3,14 @@ using System.Collections;
 
 public class Projectile : MonoBehaviour {
 
+	public float hashtagSpeed;
+	Rigidbody2D rBody;
+
 	// Use this for initialization
 	void Start () {
-	
+		rBody = GetComponent<Rigidbody2D> ();	
+
+		rBody.AddForce(new Vector2(1,0)*hashtagSpeed, ForceMode2D.Impulse);
 	}
 	
 	// Update is called once per frame
