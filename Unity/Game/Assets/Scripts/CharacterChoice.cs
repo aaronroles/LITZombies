56bb774d59f7b5193d13b76b4616cdbdc1a18001
Aaron Roles <aaron_roles93@hotmail.com>
2016-04-18 21:12:51 +0100

    Code the character choice screen

---
+++ Unity/Game/Assets/Scripts/CharacterChoice.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterChoice : MonoBehaviour {
+	public void Mike(){
+		Application.LoadLevel ("MikeGame");
+	}
+
+	public void Bernie(){
+		Application.LoadLevel ("BernieGame");
+	}
+
+	public void Richie(){
+		Application.LoadLevel ("RichieGame");
+	}
+}
